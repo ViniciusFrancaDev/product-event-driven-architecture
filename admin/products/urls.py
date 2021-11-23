2516e6095c924995aebac9c68f8dd38f5b20af8a
Vinicius Franca <vfranca14@gmail.com>
2021-11-23 08:15:23 -0300

    Adding missing line and comma

--- admin/products/urls.py
+++ admin/products/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
+
 from .views import ProductViewSet
 
 urlpatterns = [
     path('products', ProductViewSet.as_view({
         'get': 'list',
-        'post': 'create'
+        'post': 'create',
     })),
     path('products/<str:pk>', ProductViewSet.as_view({
         'get': 'retrieve',
