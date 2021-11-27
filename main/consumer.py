2e78a931e4f8bc5ded5fcb5a4645bae876109a55
Vinicius Franca <vfranca14@gmail.com>
2021-11-27 13:26:31 -0300

    Creating main consumer

---
+++ main/consumer.py
@@ -0,0 +1,21 @@
+import pika
+from pika import connection
+
+params = pika.URLParameters('your_rabbitmq_url')
+
+connection = pika.BlockingConnection(params)
+
+channel = connection.channel()
+
+channel.queue_declare(queue='main')
+
+def callback(channel, method, properties, body):
+    print('Received in main')
+    print(body)
+
+channel.basic_consume(queue='main', on_message_callback=callback)
+
+print('Started Consuming')
+
+channel.start_consuming()
+channel.close()
