7556f9340f7ad7711fdce26ac72be3831fc2b9f4
Vinicius Franca <vfranca14@gmail.com>
2021-11-29 08:26:09 -0300

    Creating callback validations to check the content type from the properties received

--- main/consumer.py
+++ main/consumer.py
@@ -1,4 +1,7 @@
-import pika
+import pika, json
+from admin.products.serializers import ProductSerializer
+
+from main import Product, db
 
 params = pika.URLParameters('your_rabbitmq_url')
 
@@ -10,7 +13,24 @@ channel.queue_declare(queue='main')
 
 def callback(channel, method, properties, body):
     print('Received in main')
-    print(body)
+    data = json.loads(body)
+    print(data)
+
+    if properties.content_type == 'product_created':
+        product = Product(id=data['id'], title=data['title'], image=data['image'])
+        db.session.add(product)
+        db.session.commit()
+
+    elif properties.content_type == 'product_updated':
+        product = Product.query.get(data['id'])
+        product.title = data['title']
+        product.image = data['image']
+        db.session.commit()
+
+    elif properties.content_type == 'product_deleted':
+        product = Product.query.get(data)
+        db.session.delete(product)
+        db.session.commit()
 
 channel.basic_consume(queue='main', on_message_callback=callback, auto_ack=True)
 
