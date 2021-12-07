28f106f630300ebd63d69fe9aaf5e53f916b5317
Vinicius Franca <vfranca14@gmail.com>
2021-12-07 08:08:12 -0300

    Adding edit route

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 import Admin from '../pages/admin/Admin.vue'
 import Products from '../pages/admin/Products.vue'
 import ProductsCreate from '../pages/admin/ProductsCreate.vue'
+import ProductsEdit from '../pages/admin/ProductsEdit.vue'
 import Main from '../pages/Main.vue'
 
 const routes: Array<RouteRecordRaw> = [
@@ -14,7 +15,8 @@ const routes: Array<RouteRecordRaw> = [
     component: Admin,
     children: [
       {path: 'products', component: Products},
-      {path: 'products/create', component: ProductsCreate}
+      {path: 'products/create', component: ProductsCreate},
+      {path: 'products/:id/edit', component: ProductsEdit}
     ]
   }
 ]
